28021cc4b0055fbab180cc334fb6ee1f40b15737
Nerval Gravie <nervalgravie@gmail.com>
2021-01-25 09:48:16 +0800

    fix splash screen

--- src/pages/Splash.js
+++ src/pages/Splash.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-import { CreateAnimation } from "@ionic/react";
+import { CreateAnimation, IonContent } from "@ionic/react";
 import './Splash.css';
 import { useHistory } from "react-router";
 
@@ -17,11 +17,11 @@ const Splash = () => {
     }
 
   return (
-    <div>
+    
       <CreateAnimation
          
          duration={1500}
-         
+
          fromTo={[
            { property: 'transform', fromValue: 'translateX(0px)', toValue: 'translateX(100px)' },
            { property: 'opacity', fromValue: '1', toValue: '0.2' }
@@ -29,8 +29,9 @@ const Splash = () => {
          easing="ease-out"
          play={playAni}
       >
+       
         <div onClick={() => {click()}} >
-
+        <IonContent fullscreen>
         <img src="assets/img/ica-slidebox-img-1.png" alt="" className="slide-image" />
             <h2 className="slide-title">
               Welcome to <b>ionic-project</b>
@@ -39,9 +40,11 @@ const Splash = () => {
               The <b>ionic-project</b> is a practical preview of the ionic framework in action, and a demonstration of proper code use.
               <b>touch</b> the screen to continue.
             </p>
+            </IonContent>
         </div>
+       
       </CreateAnimation>
-    </div>
+      
   );
 };
 
